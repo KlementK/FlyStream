71c59dcac1a501cd97dff2bd51b6d322514f90ce
Klement Kleschinsky <KlementKleschinsky@protonmail.com>
2024-03-19 19:07:37 +0200

    Huge thanks to Tim for the awesome tutorial, although many changes were needed for this code to work. - password hashing w 'bcrypt'; - new & nice project structure; - basic notepad functionality; - sign-up / login features working correctly;

---
+++ website/scripts/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+bcrypt = Bcrypt()
+
+def create_app():
+    app = Flask(__name__, template_folder='../templates')
+    app.config['SECRET_KEY'] = 'samurai'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    bcrypt.init_app(app)
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
